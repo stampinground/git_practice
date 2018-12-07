25ca92d6f5142b1f9648d14efb3f0c30b4121ca3
Stampinground <whoopee@gmail.com>
2018-12-07 08:46:23 +0000

    progressing on day2p1

--- Advent of Code/day2p1.py
+++ Advent of Code/day2p1.py
@@ -8,12 +8,19 @@ ababab'''
 
 
 input_list = day2input.splitlines()
-def counter(code_list):
-    for each_item in code_list:
-        for each_letter in each_item:
-            #if the count of letters in each item is a value do something
-            while each_item.count(each_letter) == 2 is False:
 
+def counting(code_list):
+    two_counter = 0
+    three_counter = 0
+    for each_item in code_list:
+        c = [each_item.count(i) for i in each_item]
+        if 2 in c:
+            two_counter += 1
+        elif 3 in c:
+            three_counter +=1
+    print (two_counter, three_counter)
+
+counting(input_list)
 
 
 
